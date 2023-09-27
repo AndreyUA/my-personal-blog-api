5c908ba1afdc2cf3c024ce4ebdc25358c1aca08f
AndreyUA <infernalsteel666@gmail.com>
2023-09-27 11:15:30 +0300

    created test routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -7,6 +7,26 @@ export class AppController {
 
   @Get()
   getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post()
+  postHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Delete()
+  deleteHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Put()
+  putHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Patch()
+  patchHello(): string {
     return this.appService.getHello();
   }
 }
