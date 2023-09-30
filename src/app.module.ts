67727dd9115ce4556a3e23f3f664602982cc22ab
AndreyUA <infernalsteel666@gmail.com>
2023-09-30 20:24:21 +0300

    modified npm scripts with NODE_ENV var, implemented configuration with dev, prod and test envs and added configuration file validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
+      cache: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod', 'test').required(),
+        PORT: Joi.number().required(),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
