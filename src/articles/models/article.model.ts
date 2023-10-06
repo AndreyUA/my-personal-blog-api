72c47a635f52d7dbf30850e4b941a25bff8edbbd
AndreyUA <infernalsteel666@gmail.com>
2023-10-06 21:47:48 +0300

    improved article model with id field and allow null decorators

--- src/articles/models/article.model.ts
+++ src/articles/models/article.model.ts
@@ -1,22 +1,40 @@
-import { Column, Model, Table } from 'sequelize-typescript';
+import {
+  AllowNull,
+  AutoIncrement,
+  Column,
+  Model,
+  PrimaryKey,
+  Table,
+} from 'sequelize-typescript';
 
 @Table
 export class Article extends Model {
+  @PrimaryKey
+  @AutoIncrement
+  @Column
+  id: number;
+
+  @AllowNull(false)
   @Column
   titleEn: string;
 
+  @AllowNull(false)
   @Column
   contentEn: string;
 
+  @AllowNull(false)
   @Column
   titleUa: string;
 
+  @AllowNull(false)
   @Column
   contentUa: string;
 
+  @AllowNull(false)
   @Column
   titleRu: string;
 
+  @AllowNull(false)
   @Column
   contentRu: string;
 }
