d08ac7994c88c1d95c9839164797967a73daa2d4
AndreyUA <infernalsteel666@gmail.com>
2023-10-06 22:58:38 +0300

    created routes in controller and required method in service for article crud operations

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { ArticlesService } from './articles.service';
 
 @Controller('articles')
@@ -8,5 +16,41 @@ export class ArticlesController {
   @Post()
   async createArticle() {
     return await this.articlesService.createArticle();
+  }
+
+  @Get()
+  async getArticles() {
+    return await this.articlesService.getArticles();
+  }
+
+  @Get(':id')
+  async getArticle(@Param('id') id: string) {
+    return await this.articlesService.getArticle(parseInt(id));
+  }
+
+  @Put(':id')
+  async updateArticle(@Param('id') id: string, @Body() data: any) {
+    return await this.articlesService.updateArticle(parseInt(id), data);
+  }
+
+  @Delete(':id')
+  async deleteArticle(@Param('id') id: string) {
+    return await this.articlesService.deleteArticle(parseInt(id));
+  }
+
+  @Get('/lang/:language')
+  async getArticlesByLanguage(@Param('language') language: string) {
+    return await this.articlesService.getArticlesByLanguage(language);
+  }
+
+  @Get('/lang/:language/:id')
+  async getArticleByLanguageAndId(
+    @Param('language') language: string,
+    @Param('id') id: string,
+  ) {
+    return await this.articlesService.getArticleByLanguageAndId(
+      language,
+      parseInt(id),
+    );
   }
 }
--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -22,4 +22,65 @@ export class ArticlesService {
 
     return article;
   }
+
+  async getArticles(): Promise<Article[]> {
+    const articles = await this.articleModel.findAll();
+
+    return articles;
+  }
+
+  async getArticle(id: number): Promise<Article> {
+    const article = await this.articleModel.findByPk(id);
+
+    return article;
+  }
+
+  async updateArticle(id: number, data: any): Promise<Article> {
+    const article = await this.articleModel.findByPk(id);
+    article.titleEn = data.titleEn;
+    article.contentEn = data.contentEn;
+    article.titleUa = data.titleUa;
+    article.contentUa = data.contentUa;
+    article.titleRu = data.titleRu;
+    article.contentRu = data.contentRu;
+    await article.save();
+
+    return article;
+  }
+
+  async deleteArticle(id: number): Promise<Article> {
+    const article = await this.articleModel.findByPk(id);
+    await article.destroy();
+
+    return article;
+  }
+
+  async getArticleByLanguageAndId(
+    language: string,
+    id: number,
+  ): Promise<Article> {
+    const article = await this.articleModel.findByPk(id, {
+      attributes: [
+        `title${this.capitalizeFirstLetter(language)}`,
+        `content${this.capitalizeFirstLetter(language)}`,
+      ],
+    });
+
+    return article;
+  }
+
+  async getArticlesByLanguage(language: string): Promise<Article[]> {
+    const articles = await this.articleModel.findAll({
+      attributes: [
+        `title${this.capitalizeFirstLetter(language)}`,
+        `content${this.capitalizeFirstLetter(language)}`,
+      ],
+    });
+
+    return articles;
+  }
+
+  private capitalizeFirstLetter(string: string): string {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
 }
